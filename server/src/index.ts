996e565c289d25a44619ad40b2d90ef772115f03
satish30118 <satishmaurya30118@gmail.com>
2025-04-18 15:58:53 +0530

    server setup

---
+++ server/src/index.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import app from "./app";
+import dotenv from 'dotenv'
+import connectDB from "./config/db";
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+});
